3cd44134a5b9df8e36f8d28a672b358a7992aa86
Jiger Dhillon <jigersingh29@gmail.com>
2023-02-21 18:52:50 -0500

    Patient Information

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,22 @@
     public class Program
     {
         static void Main(string[] args)
+            
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Patient Information:");
+            Console.WriteLine($"Name: {myName}  |  Age: {myAge}  |  Gender: {myGender}");
+            Console.WriteLine($"Citzen Status: {isCitzen}  |  HeartBeats/Min: {beatsPerMinute}  |  HeartBeats/Sec : {beatsPerSecond}");
         }
+        static string myName = "Jiger";
+
+        static int myAge = 20;
+
+        static char myGender = 'M';
+
+        static bool isCitzen = true;
+
+        static double beatsPerMinute = 52.4;
+
+        static decimal beatsPerSecond = .8m;
     }
 }
